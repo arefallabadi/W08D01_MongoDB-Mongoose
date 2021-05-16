20540d3b2d368c202c8f279951b96cf051731c96
arefallabadi <labadi.aref@gmail.com>
2021-05-16 12:56:42 +0300

    practice 1&2

--- app.js
+++ app.js
@@ -6,7 +6,22 @@ const app = express();
 app.use(express.json());
 
 app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
+
+
+app.post("/create/todo", (req, res) => {
+  const { task, description, deadline, isCompleted, priority } = req.body;
+  const newTodo =new todoModel({ task, description, deadline, isCompleted, priority});
+  newTodo
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+
 app.put("/update/todo", (req, res) => {});
 app.delete("/delete/todo", (req, res) => {});
 
--- schema.js
+++ schema.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
-
 const todo = new mongoose.Schema({
-    task:{type:String, required:true, unique:true},
-    description:{type:String},
-    deadline:{type:Date, default:Date.now() , required:true},
-    isCompleted:{type:String, required:true},
-    priority:{type:String}
-})
+  task: { type: String, required: true, unique: true },
+  description: { type: String },
+  deadline: { type: Number, required: true },
+  isCompleted: { type: String, required: true },
+  priority: { type: String },
+});
 
-module.exports = mongoose.model("todo",todo)
+module.exports = mongoose.model("todo", todo);
